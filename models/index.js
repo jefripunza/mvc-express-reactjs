2696c764fb6a769a1c2da180f975943211fdf66e
jefripunza <jefripunza@gmail.com>
2021-09-29 13:52:40 +0700

    editing models

--- models/index.js
+++ models/index.js
@@ -1,9 +1,18 @@
-const Database = require("./advance-json-database");
+const { Database } = require("./advance-json-database");
 const path = require("path");
 
 const database = new Database(path.join(__dirname, "database", "user.json"));
 
+// add your database function in here
+
+function addUser(data, onResult) {
+    database.add(data, result => {
+        onResult(result)
+    })
+}
+
 module.exports = {
     database,
 
+    addUser,
 }
